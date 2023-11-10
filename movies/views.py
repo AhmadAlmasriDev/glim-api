0c0d123c9c588a57d65788a06a54ec767ffc0a50
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2023-11-10 14:15:02 +0000

    Add post with permissions for movies

--- movies/views.py
+++ movies/views.py
@@ -5,13 +5,23 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Movie
 from .serializers import MovieSerializer
-
+from glim_api.permissions import ReadOnly
 
 class MovieList(APIView):
+    serializer_class = MovieSerializer
+    permission_classes = [IsAdminUser|ReadOnly]
+
     def get(self, request):
         movies = Movie.objects.all()
         serilalizer = MovieSerializer(movies, many=True, context={"request": request})
         return Response(serilalizer.data)
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data, context={"request": request})
+        if serializer.is_valid():
+            serializer.save(manager=request.user, manager_name=request.user.username)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MovieDetail(APIView):
@@ -33,7 +43,7 @@ class MovieDetail(APIView):
 
     def put(self, request, pk):
         movie = self.get_object(pk)
-        serializer = ProfileSerializer(
+        serializer = MovieSerializer(
             movie, data=request.data, context={"request": request}
         )
         if serializer.is_valid():
