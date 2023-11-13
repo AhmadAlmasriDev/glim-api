2adc973d71bd29629147c83a222bc91c56e4799e
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2023-11-13 18:57:42 +0000

    Add Ticket model

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+SEATS = 84
+
+class Ticket(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    seat = models.IntegerField( validators=[MaxValueValidator(SEATS), MinValueValidator(1)] )
+    created_at = models.DateTimeField(auto_now_add=True)
+    show_date = models.DateTimeField()
+    reserve = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.owner} movie: {self.movie} at: {self.show_date}"
