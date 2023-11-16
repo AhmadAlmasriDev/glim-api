2f50c6a727ce9fa09256dd8a32abea6734dde490
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2023-11-16 13:41:27 +0000

    Fix dj-rest-auth bug

--- glim_api/urls.py
+++ glim_api/urls.py
@@ -1,19 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
 
 urlpatterns = [
-    path('', root_route),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
-    path('dj_rest_auth/', include('dj_rest_auth.urls')),
-    path(
-        'dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')
-    ),
-    path('', include('profiles.urls')),
-    path('', include('movies.urls')),
-    path('', include('comments.urls')),
-    path('', include('likes.urls')),
-    path('', include('tickets.urls')),
-
+    path("", root_route),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("dj-rest-auth/logout/", logout_route),
+    path("dj_rest_auth/", include("dj_rest_auth.urls")),
+    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("", include("profiles.urls")),
+    path("", include("movies.urls")),
+    path("", include("comments.urls")),
+    path("", include("likes.urls")),
+    path("", include("tickets.urls")),
 ]
