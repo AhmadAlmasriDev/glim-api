29c44ca6101ac60ad0747d28aab58e10bb83c34c
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2023-12-28 20:39:57 +0000

    Deploy

--- glim_api/settings.py
+++ glim_api/settings.py
@@ -48,7 +48,8 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'glim_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'glim_api.serializers.CurrentUserSerializer',
+    'LOGIN_SERIALIZER': 'dj_rest_auth.serializers.LoginSerializer',
 }
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
