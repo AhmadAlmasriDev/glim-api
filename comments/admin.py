2d90b3b7b88cc86591133f195d4df9d0d5498fa3
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2024-01-19 21:57:29 +0000

    Add approve functionality to admin panel in comments

--- comments/admin.py
+++ comments/admin.py
@@ -2,4 +2,20 @@ from django.contrib import admin
 from .models import Comment
 
 
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'movie',
+        
+        'owner_name',
+        'created_at',
+        
+        'comment_body',
+        'approved',
+        )
+    list_filter = ('approved', 'created_at')
+    search_fields = ('user_name', 'comment_body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
