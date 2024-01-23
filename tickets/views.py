fda488dd234ff0ccbdf3858580ea974e1543f040
Ahmad Almasri <ahmad.mohd.almasri@gmail.com>
2024-01-23 14:29:55 +0000

    Add delete and put to tickets views

--- tickets/views.py
+++ tickets/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-# from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from glim_api.permissions import IsOwnerOrReadOnly
 from .models import Ticket
@@ -15,14 +14,9 @@ class TicketList(generics.ListCreateAPIView):
         serializer.save(owner=self.request.user)
 
 
-    # def list(self, request, *args, **kwargs):
-    #     queryset = self.filter_queryset(self.get_queryset())
-    #     serializer = self.get_serializer(queryset, many=True)
-    #     info_serializer = TicketInfoSerializer(queryset)
 
-    #     return Response(dict({"info" : info_serializer.data , "results": serializer.data}))
 
-class TicketDetail(generics.RetrieveAPIView):
+class TicketDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TicketSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Ticket.objects.all()
